76825854829dc36d0ca227aef37ec159891765a6
Simon Brown <simonbrown33@gmail.com>
2013-03-12 22:03:42 +0000

    New: few base files

---
+++ js/game.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+function main(argc, argv) {
+    console.log("*** Chocolate DukeNukem3D JavaScript v" + CHOCOLATE_DUKE_REV_X + "." + CHOCOLATE_DUKE_REV_DOT_Y + " ***\n\n");
+    
+    ud.multimode = 1;  // xduke: must be done before checkcommandline or that will prevent Fakeplayer and AI
+    
+    if (!load_duke3d_groupfile())
+    {
+        Error(EXIT_SUCCESS, "Could not initialize any original BASE duke3d*.grp file\n"
+							"Even if you are playing a custom GRP you still need\n"
+                        "an original base GRP file as Shareware/Full 1.3D GRP or\n"
+                        "the v1.5 ATOMIC GRP file. Such a file seems to be missing\n"
+                        "or is corrupted\n");
+    }
+}
+
+function load_duke3d_groupfile() {
+    
+}
