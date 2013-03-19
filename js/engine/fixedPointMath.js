9ab7d81fed69e5f2ea2c58d62d307cf9561481e4
Simon Brown <simonbrown33@gmail.com>
2013-03-19 17:54:30 +0000

    math now rounds same as original

--- js/engine/fixedPointMath.js
+++ js/engine/fixedPointMath.js
@@ -3,29 +3,47 @@
 function scale(input1, input2, input3) {
     return (input1 * input2 / input3) | 0;
 }
-
+// using floor so negative numbers come out the same as original
 for (var input3 = 1; input3 <= 32; input3++) {
     window["mulscale" + input3] =
-        new Function("input1", "input2", "return (input1 * input2 / Math.pow(2, " + input3 + ")) | 0");
+        new Function("input1", "input2", "return Math.floor(input1 * input2 / Math.pow(2, " + input3 + "))");
 }
 
 function mulscale(input1, input2, input3) {
-    return (input1 * input2 / Math.pow(2, input3)) | 0;
+    return Math.floor(input1 * input2 / Math.pow(2, input3));
 }
 
+//for (var input3 = 1; input3 <= 32; input3++) {
+//    window["mulscale" + input3] =
+//        new Function("input1", "input2", "return (input1 * input2) >> " + input3);
+//}
+
+//function mulscale(input1, input2, input3) {
+//    return (input1 * input2) >> input3;
+//}
+
+
+//for (var input5 = 1; input5 <= 32; input5++) {
+//    window["dmulscale" + input5] =
+//        new Function("input1", "input2", "input3", "input4", "return ((input1 * input2) + (input3 * input4)) >> " + input5);
+//}
+
+//function dmulscale(input1, input2, input3, input4, input5) {
+//    return ((input1 * input2) + (input3 * input4)) >> input5;
+//}
 
 for (var input5 = 1; input5 <= 32; input5++) {
     window["dmulscale" + input5] =
-        new Function("input1", "input2", "input3", "input4", "return (((input1 * input2) + (input3 * input4)) / Math.pow(2, " +
-            input5 + ")) | 0");
+        new Function("input1", "input2", "input3", "input4", "return Math.floor(((input1 * input2) + (input3 * input4)) / Math.pow(2, " +
+            input5 + "))");
 }
 
 function dmulscale(input1, input2, input3, input4, input5) {
-    return (((input1 * input2) + (input3 * input4)) / Math.pow(2, input5)) | 0;
+    return Math.floor(((input1 * input2) + (input3 * input4)) / Math.pow(2, input5));
 }
 
 for (var i3 = 1; i3 <= 32; i3++) {
-    window["divScale" + i3] = new Function("i1", "i2", "return (i1 * Math.pow(2, " + i3 + ") / i2) | 0;");
+    window["divScale" + i3] = new Function("i1", "i2", "return Math.floor(i1 * Math.pow(2, " + i3 + ") / i2);");
 }
 
 function divScale(i1, i2, i3) {
