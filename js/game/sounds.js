5ee37d773fdcee5d41b8dc4f2bec1e943beae1d0
Simon Brown <simonbrown33@gmail.com>
2013-03-24 19:43:01 +0000

    comments on sound/midi

--- js/game/sounds.js
+++ js/game/sounds.js
@@ -9,6 +9,12 @@ function playMusic(filename) {
 //435
 function sound() {
     // todo!
+    // https://github.com/mpruett/audiofile/blob/master/libaudiofile/VOC.cpp
+    // ?? http://seegras.discordia.ch/Programs/voc2wav
+    // https://github.com/dse/pacpl#readme
+    // https://github.com/scummvm/scummvm/blob/master/audio/decoders/voc.h
+    // https://github.com/sudocoda/jack-sox/blob/master/src/voc.c
+    // http://www.justindeltener.com/sound-programming/sound-blaster-16-tutorial/reading-the-creative-voc-sound-file-format/
 }
 
 function clearsoundlocks() {
--- js/midi/music.js
+++ js/midi/music.js
@@ -1,5 +1,12 @@
 ﻿var Music = {};
 
+// https://github.com/qiao/euphony used both these:
+// http://www.personalcopy.com/home.htm
+// https://github.com/gasman/jasmid
+
+// but it looks like MIDI.js loads soundfont plugins
+// or wait on html5 midi
+
 Music.registerTimbreBank = function() {
     console.log("registerTimbreBank ?? ");
 };
