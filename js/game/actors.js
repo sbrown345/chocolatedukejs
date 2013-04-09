513d390718a3ad913ef38ab95da3776123bb6881
Simon Brown <simonbrown33@gmail.com>
2013-04-09 20:11:06 +0100

    moveweapons

--- js/game/actors.js
+++ js/game/actors.js
@@ -153,7 +153,7 @@ function movesprite(spritenum, xchange, ychange, zchange, cliptype) {
         if (sprite[spritenum].xrepeat > 60) {
             throw "todo ref obj"
             var refObj = new R;
-            //retval = clipmove(&sprite[spritenum].x,&sprite[spritenum].y,&daz,&dasectnum,((xchange*TICSPERFRAME)<<11),((ychange*TICSPERFRAME)<<11),1024L,(4<<8),(4<<8),cliptype);
+            //retval = clipmove(sprite[spritenum].x,sprite[spritenum].y,&daz,&dasectnum,((xchange*TICSPERFRAME)<<11),((ychange*TICSPERFRAME)<<11),1024L,(4<<8),(4<<8),cliptype);
         } else {
             if (sprite[spritenum].picnum == LIZMAN)
                 cd = 292;
@@ -163,7 +163,7 @@ function movesprite(spritenum, xchange, ychange, zchange, cliptype) {
                 cd = 192;
 
             throw "todo ref obj"
-            //retval = clipmove(&sprite[spritenum].x,&sprite[spritenum].y,&daz,&dasectnum,((xchange*TICSPERFRAME)<<11),((ychange*TICSPERFRAME)<<11),cd,(4<<8),(4<<8),cliptype);
+            //retval = clipmove(sprite[spritenum].x,sprite[spritenum].y,&daz,&dasectnum,((xchange*TICSPERFRAME)<<11),((ychange*TICSPERFRAME)<<11),cd,(4<<8),(4<<8),cliptype);
         }
 
         throw "todo "
@@ -398,5 +398,355 @@ function movedummyplayers() {
         setsprite(i, sprite[i].x, sprite[i].y, sprite[i].z);
         
         i = nexti;
+    }
+}
+
+//2452
+
+function moveweapons()
+{
+    var i, j, k, nexti, p, q;
+    var dax,day,daz, x, ll;
+    var qq;
+    var s;
+  
+
+    i = headspritestat[4];
+    while(i >= 0)
+    {
+        nexti = nextspritestat[i];
+        s = sprite[i];
+
+        if(s.sectnum < 0) {deletesprite(i);i = nexti; continue;;};
+
+        hittype[i].bposx = s.x;
+        hittype[i].bposy = s.y;
+        hittype[i].bposz = s.z;
+
+        switch(s.picnum)
+        {
+            case RADIUSEXPLOSION:
+            case KNEE:
+                {deletesprite(i);i = nexti; continue;};
+            case TONGUE:
+                hittype[i].temp_data[0] = sintable[(hittype[i].temp_data[1])&2047]>>9;
+                hittype[i].temp_data[1] += 32;
+                if(hittype[i].temp_data[1] > 2047) {deletesprite(i);i = nexti; continue;};
+
+                if(sprite[s.owner].statnum == MAXSTATUS)
+                    if(badguy(sprite[s.owner]) == 0)
+                        {deletesprite(i);i = nexti; continue;};
+
+                s.ang = sprite[s.owner].ang;
+                s.x = sprite[s.owner].x;
+                s.y = sprite[s.owner].y;
+                if(sprite[s.owner].picnum == APLAYER)
+                    s.z = sprite[s.owner].z-(34<<8);
+                for(k=0;k<hittype[i].temp_data[0];k++)
+                {
+                    q = EGS(s.sectnum,
+                        s.x+((k*sintable[(s.ang+512)&2047])>>9),
+                        s.y+((k*sintable[s.ang&2047])>>9),
+                        s.z+((k*ksgn(s.zvel))*klabs(s.zvel/12)),TONGUE,-40+(k<<1),
+                        8,8,0,0,0,i,5);
+                    sprite[q].cstat = 128;
+                    sprite[q].pal = 8;
+                }
+                q = EGS(s.sectnum,
+                    s.x+((k*sintable[(s.ang+512)&2047])>>9),
+                    s.y+((k*sintable[s.ang&2047])>>9),
+                    s.z+((k*ksgn(s.zvel))*klabs(s.zvel/12)),INNERJAW,-40,
+                    32,32,0,0,0,i,5);
+                sprite[q].cstat = 128;
+                if( hittype[i].temp_data[1] > 512 && hittype[i].temp_data[1] < (1024) )
+                    sprite[q].picnum = INNERJAW+1;
+
+                i = nexti; continue;
+
+            case FREEZEBLAST:
+                if(s.yvel < 1 || s.extra < 2 || (s.xvel|s.zvel) == 0)
+                {
+                    j = spawn(i,TRANSPORTERSTAR);
+                    sprite[j].pal = 1;
+                    sprite[j].xrepeat = 32;
+                    sprite[j].yrepeat = 32;
+                    {deletesprite(i);i = nexti; continue;};
+                }
+            case SHRINKSPARK:
+            case RPG:
+            case FIRELASER:
+            case SPIT:
+            case COOLEXPLOSION1:
+
+                if( s.picnum == COOLEXPLOSION1 )
+                    if( Sound[WIERDSHOT_FLY].num == 0 )
+                        spritesound(WIERDSHOT_FLY,i);
+
+                p = -1;
+
+                if(s.picnum == RPG && sector[s.sectnum].lotag == 2)
+                {
+                    k = s.xvel>>1;
+                    ll = s.zvel>>1;
+                }
+                else
+                {
+                    k = s.xvel;
+                    ll = s.zvel;
+                }
+
+                dax = s.x; day = s.y; daz = s.z;
+
+                getglobalz(i);
+                qq = CLIPMASK1;
+
+                switch(s.picnum)
+                {
+                    case RPG:
+                        if(hittype[i].picnum != BOSS2 && s.xrepeat >= 10 && sector[s.sectnum].lotag != 2)
+                        {
+                            j = spawn(i,SMALLSMOKE);
+                            sprite[j].z += (1<<8);
+                        }
+                        break;
+                }
+
+                j = movesprite(i,
+                    (k*(sintable[(s.ang+512)&2047]))>>14,
+                    (k*(sintable[s.ang&2047]))>>14,ll,qq);
+
+                if(s.picnum == RPG && s.yvel >= 0)
+                    if( FindDistance2D(s.x-sprite[s.yvel].x,s.y-sprite[s.yvel].y) < 256 )
+                        j = 49152|s.yvel;
+
+                if(s.sectnum < 0) { {deletesprite(i);i = nexti; continue;}; }
+
+                if( (j&49152) != 49152)
+                    if(s.picnum != FREEZEBLAST)
+                    {
+                        if(s.z < hittype[i].ceilingz)
+                        {
+                            j = 16384|(s.sectnum);
+                            s.zvel = -1;
+                        }
+                        else
+                            if( ( s.z > hittype[i].floorz && sector[s.sectnum].lotag != 1 ) ||
+                                ( s.z > hittype[i].floorz+(16<<8) && sector[s.sectnum].lotag == 1 ) )
+                            {
+                                j = 16384|(s.sectnum);
+                                if(sector[s.sectnum].lotag != 1)
+                                    s.zvel = 1;
+                            }
+                    }
+
+                if(s.picnum == FIRELASER)
+                {
+                    for(k=-3;k<2;k++)
+                    {
+                        x = EGS(s.sectnum,
+                            s.x+((k*sintable[(s.ang+512)&2047])>>9),
+                            s.y+((k*sintable[s.ang&2047])>>9),
+                            s.z+((k*ksgn(s.zvel))*klabs(s.zvel/24)),FIRELASER,-40+(k<<2),
+                            s.xrepeat,s.yrepeat,0,0,0,s.owner,5);
+
+                        sprite[x].cstat = 128;
+                        sprite[x].pal = s.pal;
+                    }
+                }
+                else if(s.picnum == SPIT) if(s.zvel < 6144)
+                    s.zvel += gc-112;
+
+                if( j != 0 )
+                {
+                    if(s.picnum == COOLEXPLOSION1)
+                    {
+                        if( (j&49152) == 49152 && sprite[j&(MAXSPRITES-1)].picnum != APLAYER)
+                            i = nexti; continue;
+                        s.xvel = 0;
+                        s.zvel = 0;
+                    }
+
+                    if( (j&49152) == 49152 )
+                    {
+                        j &= (MAXSPRITES-1);
+
+                        if(s.picnum == FREEZEBLAST && sprite[j].pal == 1 )
+                            if( badguy(sprite[j]) || sprite[j].picnum == APLAYER )
+                            {
+                                j = spawn(i,TRANSPORTERSTAR);
+                                sprite[j].pal = 1;
+                                sprite[j].xrepeat = 32;
+                                sprite[j].yrepeat = 32;
+
+                                {deletesprite(i);i = nexti; continue;};
+                            }
+
+                        checkhitsprite(j,i);
+
+                        if(sprite[j].picnum == APLAYER)
+                        {
+                            p = sprite[j].yvel;
+                            spritesound(PISTOL_BODYHIT,j);
+
+                            if(s.picnum == SPIT)
+                            {
+                                ps[p].horiz += 32;
+                                ps[p].return_to_center = 8;
+
+                                if(ps[p].loogcnt == 0)
+                                {
+                                    if(Sound[DUKE_LONGTERM_PAIN].num < 1)
+                                        spritesound(DUKE_LONGTERM_PAIN,ps[p].i);
+
+                                    j = 3+(krand()&3);
+                                    ps[p].numloogs = j;
+                                    ps[p].loogcnt = 24*4;
+                                    for(x=0;x < j;x++)
+                                    {
+                                        ps[p].loogiex[x] = krand()%xdim;
+                                        ps[p].loogiey[x] = krand()%ydim;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if( (j&49152) == 32768 )
+                    {
+                        j &= (MAXWALLS-1);
+
+                        if(s.picnum != RPG && s.picnum != FREEZEBLAST && s.picnum != SPIT && ( wall[j].overpicnum == MIRROR || wall[j].picnum == MIRROR ) )
+                        {
+                            k = getangle(
+                                    wall[wall[j].point2].x-wall[j].x,
+                                    wall[wall[j].point2].y-wall[j].y);
+                            s.ang = ((k<<1) - s.ang)&2047;
+                            s.owner = i;
+                            spawn(i,TRANSPORTERSTAR);
+                            i = nexti; continue;
+                        }
+                        else
+                        {
+                            setsprite(i,dax,day,daz);
+                            checkhitwall(i,j,s.x,s.y,s.z,s.picnum);
+
+                            if(s.picnum == FREEZEBLAST)
+                            {
+                                if( wall[j].overpicnum != MIRROR && wall[j].picnum != MIRROR )
+                                {
+                                    s.extra >>= 1;
+                                    s.yvel--;
+                                }
+
+                                k = getangle(
+                                    wall[wall[j].point2].x-wall[j].x,
+                                    wall[wall[j].point2].y-wall[j].y);
+                                s.ang = ((k<<1) - s.ang)&2047;
+                                i = nexti; continue;
+                            }
+                        }
+                    }
+                    else if( (j&49152) == 16384)
+                    {
+                        setsprite(i,dax,day,daz);
+
+                        if(s.zvel < 0)
+                        {
+                            if( sector[s.sectnum].ceilingstat&1 )
+                                if(sector[s.sectnum].ceilingpal == 0)
+                                    {deletesprite(i);i = nexti; continue;};
+
+                            checkhitceiling(s.sectnum);
+                        }
+
+                        if(s.picnum == FREEZEBLAST)
+                        {
+                            bounce(i);
+                            ssp(i,qq);
+                            s.extra >>= 1;
+                            if(s.xrepeat > 8)
+                                s.xrepeat -= 2;
+                            if(s.yrepeat > 8)
+                                s.yrepeat -= 2;
+                            s.yvel--;
+                            i = nexti; continue;
+                        }
+                    }
+
+                    if(s.picnum != SPIT)
+                    {
+                        if(s.picnum == RPG)
+                        {
+                            k = spawn(i,EXPLOSION2);
+                            sprite[k].x = dax;
+                            sprite[k].y = day;
+                            sprite[k].z = daz;
+
+                            if(s.xrepeat < 10)
+                            {
+                                sprite[k].xrepeat = 6;
+                                sprite[k].yrepeat = 6;
+                            }
+                            else if( (j&49152) == 16384)
+                            {
+                                if( s.zvel > 0)
+                                    spawn(i,EXPLOSION2BOT);
+                                else { sprite[k].cstat |= 8; sprite[k].z += (48<<8); }
+                            }
+                        }
+                        else if(s.picnum == SHRINKSPARK)
+                        {
+                            spawn(i,SHRINKEREXPLOSION);
+                            spritesound(SHRINKER_HIT,i);
+                            hitradius(i,shrinkerblastradius,0,0,0,0);
+                        }
+                        else if( s.picnum != COOLEXPLOSION1 && s.picnum != FREEZEBLAST && s.picnum != FIRELASER)
+                        {
+                            k = spawn(i,EXPLOSION2);
+                            sprite[k].xrepeat = sprite[k].yrepeat = s.xrepeat>>1;
+                            if( (j&49152) == 16384)
+                            {
+                                if( s.zvel < 0)
+                                { sprite[k].cstat |= 8; sprite[k].z += (72<<8); }
+                            }
+                        }
+                        if( s.picnum == RPG )
+                        {
+                            spritesound(RPG_EXPLODE,i);
+
+                            if(s.xrepeat >= 10)
+                            {
+                                x = s.extra;
+                                hitradius( i,rpgblastradius, x>>2,x>>1,x-(x>>2),x);
+                            }
+                            else
+                            {
+                                x = s.extra+(global_random&3);
+                                hitradius( i,(rpgblastradius>>1),x>>2,x>>1,x-(x>>2),x);
+                            }
+                        }
+                    }
+                    if(s.picnum != COOLEXPLOSION1) {deletesprite(i);i = nexti; continue;};
+                }
+                if(s.picnum == COOLEXPLOSION1)
+                {
+                    s.shade++;
+                    if(s.shade >= 40) {deletesprite(i);i = nexti; continue;};
+                }
+                else if(s.picnum == RPG && sector[s.sectnum].lotag == 2 && s.xrepeat >= 10 && rnd(140))
+                    spawn(i,WATERBUBBLE);
+
+                i = nexti; continue;
+
+
+            case SHOTSPARK1:
+                var xRef = new Ref(x);
+                p = findplayer(s, xRef);
+                x = xRef.$;
+                execute(i,p,x);
+                i = nexti; continue;
+        }
+        //BOLT:
+            i = nexti;
+
     }
 }
