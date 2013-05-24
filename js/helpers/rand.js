e4ead0a16bc5e65d7fe0030191118d2b70cdb3b9
Simon Brown <simonbrown33@gmail.com>
2013-05-24 21:59:09 +0100

    bonus kinda works but too fast

--- js/helpers/rand.js
+++ js/helpers/rand.js
@@ -1,6 +1,8 @@
 ﻿'use strict';
 
+Object.defineProperty(window, "NORMAL_RAND", rand); // used for comparing original
+
 function rand() {
     console.log("todo:  rand - make same as c version for testing");
     return Math.random()*10000|0;
-}
+} 
